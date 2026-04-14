83e1d2c57cb6bf46fded7476cd636cb6dae2b428
Mark Lifantsev <mark.lifantsev@gmail.com>
2026-04-14 00:22:04 -0700

    gpg: set default pinentry to my custom one

--- home/gpg.nix
+++ home/gpg.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }@args: {
+{ ... }@args: {
     programs.gpg.enable = true;
 
     # NOTE after changing anything, run `gpgconf --reload gpg-agent` to apply
@@ -8,16 +8,10 @@
     in {
         enable = true;
 
-        pinentry.package = pkgs.pinentry-qt;
-        # pinentry.package = (import ../pkgs/import.nix args) ../pkgs/apps/pinentry-pypr;
-        # pinentry.package = (import ../pkgs/import.nix args) ../pkgs/apps/pinentry-bash;
+        pinentry.package = (import ../pkgs/import.nix args) ../pkgs/apps/pinentry-pypr;
 
-        # TODO remove logging
         extraConfig = ''
             allow-loopback-pinentry
-            log-file /tmp/gpg-agent.log
-            debug-level advanced
-            debug-pinentry
         '';
 
         enableSshSupport = true;
@@ -30,6 +24,4 @@
         maxCacheTtl = timeout;
         maxCacheTtlSsh = timeout;
     };
-
-    qt.platformTheme = "gtk2"; # gtk theme in cursor-and-gtk.nix
 }
