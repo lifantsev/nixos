6baa1dfae41235dda80c2224981ab885330a5c4f
Mark Lifantsev <mark.lifantsev@gmail.com>
2026-04-18 04:45:11 -0700

    updated lifantsev-nixvim to version that uses hjkl by default (so i override with naio)

--- home/nvim.nix
+++ home/nvim.nix
@@ -4,5 +4,20 @@
         colorscheme = rice.col.name;
         colors = lib.mapAttrs (color: attrs: attrs.h) # extract hex code
                 (lib.filterAttrs (k: v: builtins.typeOf v == "attrs") rice.col); # filter out metadata
+
+        keys = {
+            directional = {
+                left  = "n";
+                up    = "a";
+                down  = "i";
+                right = "o";
+            };
+            hjkl = {
+                h = "i";
+                j = "n";
+                k = "a";
+                l = "o";
+            };
+        };
     };
 }
