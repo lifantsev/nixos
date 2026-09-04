d49a3cb21b09994094e3443d041cf1d59394fef7
Mark Lifantsev <mark.lifantsev@gmail.com>
2026-09-04 11:15:48 -0700

    niri: add binds to launch pass-autotype

--- home/niri/binds/spawn.nix
+++ home/niri/binds/spawn.nix
@@ -1,9 +1,13 @@
-{ spawn, ... }: {
+{ spawn, sh, ... }: {
     default = {
         T = spawn "kitty";
         H = spawn "qutebrowser";
+        B = sh "brave --ozone-platform=wayland";
 
         Z = spawn "screenshot";
         SHIFT.Z = spawn "screenrecord";
+
+        P = spawn "pass-autotype";
+        SHIFT.P = sh "pass-autotype --interactive";
     };
 }
