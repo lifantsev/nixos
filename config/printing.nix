3c0772107791a9b38e273d9dfc0d46a293106f08
Mark Lifantsev <mark.lifantsev@gmail.com>
2026-09-04 11:14:58 -0700

    get printing to work on m1 pro w/ brother printer

--- config/printing.nix
+++ config/printing.nix
@@ -1,12 +1,16 @@
 { pkgs, ... } : {
+    # to add a printer go to http://localhost:631/admin
+    # driver to use: HL-1250 (with brother HL-2170w printer)
+
     # enable this stuff when needed
-    # services.printing = {
-    #     enable = true;
-    #     drivers = [
-    #         pkgs.gutenprint # brother uses the hl1250 drivers
-    #         pkgs.gutenprintBin
-    #         pkgs.hplip
-    #         pkgs.hplipWithPlugin
-    #     ];
-    # };
+    services.printing = {
+        enable = true;
+        drivers = [ pkgs.gutenprint ];
+    };
+
+    services.avahi = {
+        enable = true;
+        nssmdns4 = true;
+        openFirewall = true;
+    };
 }
