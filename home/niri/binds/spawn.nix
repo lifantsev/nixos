b5b0ec25d48d4963b238ae9e55bca625d1de3d7f
Mark Lifantsev <mark.lifantsev@gmail.com>
2026-09-09 00:31:04 -0700

    kitty & niri: add a bind to spawn a new kitty window with the same cwd

--- home/niri/binds/spawn.nix
+++ home/niri/binds/spawn.nix
@@ -1,6 +1,8 @@
 { spawn, sh, ... }: {
     default = {
         T = spawn "kitty";
+        SHIFT.T = sh "wtype -M ctrl -M alt -M shift -k t -m shift -m alt -m ctrl"; # kitty spawn window with same cwd
+
         H = spawn "qutebrowser";
         B = sh "brave --ozone-platform=wayland";
         X = spawn "zen-beta";
