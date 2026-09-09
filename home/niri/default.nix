7b1398c5c33e2dc6dd502848a5ba124c258a9edb
Mark Lifantsev <mark.lifantsev@gmail.com>
2026-09-08 22:52:08 -0700

    niri: cleanup: move dropdowns.nix to config/

--- home/niri/default.nix
+++ home/niri/default.nix
@@ -24,7 +24,7 @@
 
         niridrop = {
             enable = true;
-            windows = import ./dropdowns.nix args;
+            windows = import ./config/dropdowns.nix args;
         };
     };
 };}
