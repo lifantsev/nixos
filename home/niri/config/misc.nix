8cc19e46adfe608e4347e76809fe144e2033c8cc
Mark Lifantsev <mark.lifantsev@gmail.com>
2026-09-08 20:56:00 -0700

    niri: disable alt-tab switcher

--- home/niri/config/misc.nix
+++ home/niri/config/misc.nix
@@ -2,4 +2,5 @@
     debug.render-drm-device = "/dev/dri/renderD128";
     screenshot-path = null;
     hotkey-overlay.skip-at-startup = true;
+    recent-windows.enable = false; # alt-tab switcher
 }
