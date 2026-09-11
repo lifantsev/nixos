e6599a535778eef571146bf88746eaf6c6c3db04
Mark Lifantsev <mark.lifantsev@gmail.com>
2026-09-10 22:38:34 -0700

    niri: made terminal & browser keybinds dependent on variables

--- home/niri/binds/apps.nix
+++ home/niri/binds/apps.nix
@@ -1,11 +1,15 @@
-{ spawn, sh, ... }: {
-    default = {
-        T = spawn "kitty";
+{ func = { config, ... }: { spawn, sh, ... }: {
+    default = let
+        term = config.home.sessionVariables.TERMINAL;
+        browser = config.home.sessionVariables.BROWSER;
+    in {
+        H = spawn browser;
+        T = spawn term;
         SHIFT.T = sh "wtype -M ctrl -M alt -M shift -k t -m shift -m alt -m ctrl"; # kitty spawn window with same cwd
 
-        H = spawn "zen-beta";
         B = spawn "obsidian";
 
+        # utilities
         Z = spawn "screenshot";
         SHIFT.Z = spawn "screenrecord";
 
@@ -14,4 +18,4 @@
 
         Space = sh "makoctl dismiss -a";
     };
-}
+};}
